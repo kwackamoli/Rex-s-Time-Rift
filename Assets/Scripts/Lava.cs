bdac9a88210a8f1ddb4a8229350f7b1683530b23
Jesakah <109999420+swag-master0@users.noreply.github.com>
2024-08-10 17:16:59 +1200

    Level one and player movement

---
+++ Assets/Scripts/Lava.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Lava : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject.name == "Player"){
+            Score.pickupsCollected = 0;
+            SceneManager.LoadScene("Floor One");
+        }
+    }
+}
