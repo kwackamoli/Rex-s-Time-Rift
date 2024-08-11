332aebc70b39bab0a307425a2c64c4243e6c9fd4
swag-master0 <109999420+swag-master0@users.noreply.github.com>
2024-08-11 16:54:15 +1200

    chuckles

--- Assets/Scripts/Audio.cs
+++ Assets/Scripts/Audio.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Audio : MonoBehaviour
 {
     public AudioSource audio;
+    public int chuckles;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,7 @@ public class Audio : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Random.Range(0,3000)==0){
+        if (Random.Range(0,chuckles)==0){
 audio.Play();
         }
     }
